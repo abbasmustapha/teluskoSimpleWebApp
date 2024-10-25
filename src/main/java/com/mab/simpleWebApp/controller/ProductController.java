15336aa711f6527fbfee35947cca056ec881e232
mostafa <abbas.mustapha@gmail.com>
2024-10-26 00:52:55 +0200

    FIN tuto 15 Spring Web Http methods GET and POST

--- src/main/java/com/mab/simpleWebApp/controller/ProductController.java
+++ src/main/java/com/mab/simpleWebApp/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.mab.simpleWebApp.controller;
 import com.mab.simpleWebApp.model.Product;
 import com.mab.simpleWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,9 +13,19 @@ public class ProductController {
     @Autowired
     ProductService service;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts(){
         return service.getProducts();
+    }
+
+    @GetMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId){
+        return service.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product prod){
+        service.addProduct(prod);
     }
 
 }
--- src/main/java/com/mab/simpleWebApp/service/ProductService.java
+++ src/main/java/com/mab/simpleWebApp/service/ProductService.java
@@ -3,21 +3,31 @@ package com.mab.simpleWebApp.service;
 import com.mab.simpleWebApp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
-            new Product(1,"iPhone",1400),
-            new Product(2,"TV",700),
-            new Product(3,"Mouse",26),
-            new Product(4,"Tablette",390)
-    );
+    List<Product> products = new ArrayList<>(Arrays.asList(
+            new Product(100,"iPhone",1400),
+            new Product(120,"TV",700),
+            new Product(130,"Mouse",26),
+            new Product(140,"Tablette",390)));
 
     public List<Product> getProducts(){
         return products;
+    }
+
+    public Product getProductById(int prodId) {
+        return products.stream()
+                .filter(p -> p.getProdId() == prodId)
+                .findFirst().orElse(new Product(999,"Pas trouvé",0));
+    }
+
+    public void addProduct(Product prod){
+        products.add(prod);
     }
 
 }
